d1424474c03cefa1bfb6b18b55f7f0ea2d658cf3
Avram Paul <147730542+PateuVegetal@users.noreply.github.com>
2023-12-13 20:08:29 +0200

    program functional
    acum merge

--- knight.cpp
+++ knight.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
-int Matrix[8][8]={0};
-int x[8]={2, 1, -1, -2, -2, -1, 1, 2};
-int y[8]={1, 2, 2, 1, -1, -2, -2, -1};
-int contor=0;
+int Matrix[8][8] = {0};
+int x[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+int y[8] = {1, 2, 2, 1, -1, -2, -2, -1};
 
 int este_valid(int i, int j)
 {
-     if(i >= 0 && i < 8 && j >= 0 && j < 8 && Matrix[i][j]==0)
-        return 1;
-    return 0;
+    return (i >= 0 && i < 8 && j >= 0 && j < 8 && Matrix[i][j] == -1);
+}
+
+void initializare_matrice()
+{
+    for(int i = 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
+            Matrix[i][j] = -1;
 }
 
 void afisare_matrice()
@@ -23,39 +27,48 @@ void afisare_matrice()
             cout << Matrix[i][j] << "|";
         cout << "\n";
     }
-    cout<<"\n\n\n";
+    cout << "\n\n\n";
 }
 
 int knights_tour(int m, int n, int contor)
 {
-    int i;
-
-    for(i=0; i<8; i++)
+    if (contor == 64)
     {
-        if(contor >= 65)
-            return 1;
+        afisare_matrice();
+        return 1;
+    }
 
-        int poz_i=m+x[i];
-        int poz_j=n+y[i];
-        if(este_valid(poz_i, poz_j))
+    for (int i = 0; i < 8; i++)
+    {
+        int poz_i = m + x[i];
+        int poz_j = n + y[i];
+        if (este_valid(poz_i, poz_j))
         {
-            Matrix[poz_i][poz_j]=contor;
-            afisare_matrice();
-            if(knights_tour(poz_i, poz_j, contor+1))
-                {
-                    return 1;
-                }
-            Matrix[poz_i][poz_j]=0;
+            Matrix[poz_i][poz_j] = contor;
+            if (knights_tour(poz_i, poz_j, contor + 1))
+            {
+                return 1;
+            }
+            else
+            {
+                Matrix[poz_i][poz_j] = -1;
+            }
         }
     }
     return 0;
 }
 
-
-
 int main()
 {
-    knights_tour(3, 3, 0);
+    int i, j;
+    initializare_matrice();
 
+    cout << "Scrieti pozitia de pe care vreti sa incepeti: \n i(0-7): ";
+    cin >> i;
+    cout << "j(0-7): ";
+    cin >> j;
+    Matrix[i][j] = 0;
+    knights_tour(i, j, 1);
+    afisare_matrice();
     return 0;
 }
